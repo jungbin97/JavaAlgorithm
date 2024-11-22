28774515fd453d637fb6e7a275de1f2b94bcf179
ego2 <57621519+jungbin97@users.noreply.github.com>
2024-11-22 17:57:10 +0900

    [Silver III] Title: 소수 구하기, Time: 756 ms, Memory: 37600 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        
+        int[] A = new int[n+1];
+        for (int i = 2; i <= n; i++) {
+            A[i] = i;
+        }
+        
+        for (int i = 2; i <= n; i++) {
+            if (A[i] == 0) {
+                continue;
+            }
+            for (int j = i+i; j <= n; j = j+i) {
+                A[j] = 0;
+            }
+        }
+        
+        for (int i = m; i <= n; i++) {
+            if (A[i] != 0) {
+                System.out.println(A[i]);
+            } 
+        }
+    }
+}
