b7841e4c4a754570cb5977377bf0c9e790be1111
ego2 <57621519+jungbin97@users.noreply.github.com>
2025-01-07 08:00:16 +0900

    [level 2] Title: 올바른 괄호, Time: 17.14 ms, Memory: 54.7 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -5,21 +5,19 @@ class Solution {
         boolean answer = true;
 		Stack<Character> stack = new Stack<>();
         
-        char[] arr = s.toCharArray();
-        for (char c : arr) {
-            // 열린괄호
-            if (c == '(')  {
-                stack.push(c);
+        //char[] arr = s.toCharArray();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(')  {
+                stack.push('(');
             } else {
                 if (stack.isEmpty()) {
                     return false;
-                } else {
-                    stack.pop();
                 }
+                stack.pop();
             }
         }
         
-        if (!stack.empty()) {
+        if (!stack.isEmpty()) {
             return false;
         }
 
