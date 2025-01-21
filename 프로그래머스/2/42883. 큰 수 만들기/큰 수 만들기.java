b805a123081aab849b8214a5d856498801358ee7
ego2 <57621519+jungbin97@users.noreply.github.com>
2025-01-21 22:35:17 +0900

    [level 2] Title: 큰 수 만들기, Time: 7678.64 ms, Memory: 92.6 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -2,51 +2,30 @@ import java.util.*;
 
 class Solution {
     public String solution(String number, int k) {
-        Stack<Character> stack = new Stack<>();
         String answer = "";
-        
-        for (int i = 0; i < number.length(); i++) {
-            char nowNum = number.charAt(i);
-            
-            while (k > 0 && !stack.isEmpty() && stack.peek() < nowNum) {
-                stack.pop();
-                k--;
-            }
-            stack.push(nowNum);
-        }
-        
-        // k가 남은 경우: 54321
-        // 뒤에서 남은 K만큼 제거
-        if (k > 0) {
-            stack.pop();
-            k--;
-        }
-        
         StringBuilder sb = new StringBuilder();
-        for(char c : stack) {
-            sb.append(c);
+        
+        char[] array = number.toCharArray();
+        
+        // 구해야하는 문자 길이
+        int len = array.length - k;
+        
+        int start = 0;
+        for (int i = 0; i < len; i++) {
+            char max = '0';
+            
+            for (int j = start; j <= i + k; j++) {
+                // 가장 큰수 고르기, 그 다음 인덱스 시작 인덱스
+                if (array[j] > max) {
+                    max = array[j];
+                    start = j+1;
+                }
+            }
+            sb.append(max);
         }
         
         return sb.toString();
     }
 }
 
-
-
-// 1924, k=2
-// 1, 2제거 94
-
-// 1231234, k=3
-// 1, 2, 1제거 3234
-
-
-// stack = [1]
-// stack = [2], stack.peek() = 1 < 2,stack.pop(1) stack.append(2) 
-// stack = [3], stack.peek() = 2 < 3,stack.pop(2) stack.append(3)
-// stack = [3, 1] stack.peek() > 1, stack.append(1)
-// stack = [3, 2] stack.peek() < 2, stack.pop(1), stack.append(2)
-
-// k = 0 => 3234
-
-
-// k == 0인 경우 나머지 문자 쭉 추가하기
+// 그리디 하기 풀기
