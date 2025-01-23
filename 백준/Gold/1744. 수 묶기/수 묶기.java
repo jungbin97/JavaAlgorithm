4e3756ee807ffb412b49875cf67f85bd60fbd2b2
ego2 <57621519+jungbin97@users.noreply.github.com>
2025-01-23 17:30:14 +0900

    [Gold IV] Title: 수 묶기, Time: 108 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Gold/1744. 수 묶기/수 묶기.java
+++ 백준/Gold/1744. 수 묶기/수 묶기.java
@@ -3,67 +3,77 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
+        // 내림차순
+        PriorityQueue<Integer> plusPQ = new PriorityQueue<>(Collections.reverseOrder());
+        // 오름차순
+        PriorityQueue<Integer> minusPQ = new PriorityQueue<>();
         
-        int n  = sc.nextInt();
-
-        // 양수, 음수, 1, 0
-        // 양수는 내림차순으로 
-        // 음수는 오름차순으로
-        PriorityQueue<Integer> plusQ = new PriorityQueue<>(Collections.reverseOrder());
-        PriorityQueue<Integer> minusQ = new PriorityQueue<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
         int one = 0;
         int zero = 0;
+        
+        int result = 0;
 
-
-        // 입력값 4가지로 분리 저장
-        for (int i = 0; i < n; i++) {
-            int number = sc.nextInt();
-            
-            // 양수 일때
-            if (number > 1) {
-                plusQ.add(number);
-            } else if (number == 0) {
-                zero++;
-            } else if (number == 1) {
+        int N = Integer.parseInt(br.readLine());
+        
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(br.readLine());
+            // 양수
+            if (num>1) {
+                plusPQ.add(num);
+            } else if (num == 1) {
                 one++;
+            } else if (num == 0) {
+                zero++;
             } else {
-                minusQ.add(number);
+                minusPQ.add(num);
             }
         }
         
-        int sum = 0;
-        //  양수 처리
-        while (plusQ.size() > 1) {
-            int first = plusQ.poll();
-            int second = plusQ.poll();
-            sum = sum + first * second;
-        }
+        // 양수 처리
+        while (plusPQ.size() > 1) {
+            int first = plusPQ.poll();
+            int second = plusPQ.poll();
 
-        // 큐에 남아있으면
-        if (!plusQ.isEmpty()) {
-            sum = sum + plusQ.poll();
+            result += (first * second);
         }
         
-        // 음수 처리 하기
-        while (minusQ.size() > 1) {
-            int first = minusQ.poll();
-            int second = minusQ.poll(); 
-            sum = sum + (first * second);
+        // 남은 수 처리
+        if (!plusPQ.isEmpty()) {
+            result += plusPQ.poll();
         }
-    
-        // 큐에 남아있으면 
-        if (!minusQ.isEmpty()) {
-            if (zero == 0) {
-                sum = sum + minusQ.poll();
+        
+        // 음수처리
+        while (minusPQ.size() > 1) {
+            int first = minusPQ.poll();
+            int second = minusPQ.poll();
+
+            result += (first * second);
+        }
+        
+        // 남은 수 처리
+        if(!minusPQ.isEmpty()) {
+            if (zero > 0) {
+                result += minusPQ.poll() * 0;
+            } else {
+                result += minusPQ.poll();
             }
         }
         
-        // 1 처리하기
-        sum = sum + one;
-        System.out.println(sum);
-        
+        // 1처리 후 반환
+        System.out.println(result + one);
 
     }
 }
+
+// 0 개별 카운트
+// 1 개별 카운트
+// 
+// 음수 => 오름차순
+// 나머지 하나 남으면(홀수)이면, 0과 곱하기, 0이 없으면 그냥 더하기
+
+// 남은 1은 그냥 더하기
+
+// 양수 => 내림차순 
+// 홀수개면 마지막 그냥 더하기
