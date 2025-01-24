419f84eb2a1984501e2a73a486510349c897be5d
ego2 <57621519+jungbin97@users.noreply.github.com>
2025-01-24 17:47:44 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 504 ms, Memory: 48356 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,56 +1,74 @@
 import java.util.*;
 import java.io.*;
-import java.net.SocketTimeoutException;
 
 public class Main {
     static int[] parent;
     static PriorityQueue<Edge> pq = new PriorityQueue<>();
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
         
-        // 노드 개수
-        int V = sc.nextInt();
-        // 에지 개수
-        int E = sc.nextInt();
-        
-        // 부모 배열(union-find)
+        // 유니온 파인드 배열 초기화
         parent = new int[V+1];
-        for(int i = 1; i<=V; i++) {
+        
+        for(int i = 1; i <= V; i++) {
             parent[i] = i;
         }
-        
 
-        // 에지 리스트 초기화(오름차순으로 정렬)
+
         for (int i = 0; i < E; i++) {
-            int s = sc.nextInt();
-            int e = sc.nextInt();
-            int v = sc.nextInt();
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            
             pq.add(new Edge(s, e, v));
         }
         
         int useEdge = 0;
         int result = 0;
-        
-        while (useEdge < V-1) {
-            // 현재 가장 작은 엣지
-            Edge now = pq.poll();
+        while(useEdge < V-1) {
+            Edge edge = pq.poll();
             
-            if (find(now.s) != find(now.e)) {
-                union(now.s, now.e);
-                result = result + now.v;
+            if (find(edge.s) != find(edge.e)) {
+                union(edge.s, edge.e);
                 useEdge++;
+                result = result + edge.v;
             }
         }
+        
         System.out.println(result);
         
     }
+    
+    static int find(int a) {
+        if (parent[a] == a) {
+            return a;
+        }
 
-    public static class Edge implements Comparable<Edge> {
+        return parent[a] = find(parent[a]);
+    }
+    
+    static void union (int a, int b) {
+        a = find(a);
+        b = find(b);
+
+        if (a!=b) {
+            parent[b] = a;
+        }
+    }
+    
+
+
+    static class Edge implements Comparable<Edge> {
         int s;
         int e;
         int v;
 
-        public Edge(int s, int e, int v) {
+        Edge (int s, int e, int v) {
             this.s = s;
             this.e = e;
             this.v = v;
@@ -58,29 +76,8 @@ public class Main {
 
         @Override
         public int compareTo(Edge o) {
-            return Integer.compare(this.v, o.v);
+            return (this.v - o.v);
         }
     }
-    
-    // find
-    public static int find(int a) {
-        // 자기 자신이면
-        if (a == parent[a]) {
-            return a;
-        } else {
-            // 경로 압축
-            return parent[a] = find(parent[a]);
-        }
 
-    }
-    
-    // union
-    public static void union(int a, int b) {
-        a = find(a);
-        b = find(b);
-        
-        if (a != b) {
-            parent[b] = a;
-        }
-    }
 }
