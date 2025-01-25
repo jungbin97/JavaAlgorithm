509d3a7b02e904aaaddec1bf19f9d0446eec58ed
ego2 <57621519+jungbin97@users.noreply.github.com>
2025-01-25 21:07:36 +0900

    [level 3] Title: 가장 먼 노드, Time: 61.12 ms, Memory: 98 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -2,7 +2,9 @@ import java.util.*;
 
 class Solution {
     static ArrayList<Integer>[] graph;
-    static int[] result;
+    static int[] distance;
+    static boolean[] visited;
+    static PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]); 	//정렬 기준 가장 작은 비용
     public int solution(int n, int[][] edge) {
         int answer = 0;
         
@@ -10,7 +12,6 @@ class Solution {
         for(int i = 0; i <= n; i++) {
             graph[i] = new ArrayList<>();
         }
-        
         
         // 엣지 간접 리스트 초기화
         for (int i = 0; i < edge.length; i++) {
@@ -20,58 +21,54 @@ class Solution {
             graph[end].add(start);
         }
         
-        boolean[] visited = new boolean[n+1];
-        result = new int[n+1];
+        visited = new boolean[n+1];
         
-        bfs(visited, 1);
-        
-        //for (int i = 1; i < result.length; i++) {
-        //    System.out.println(i + " 노드의 최소 간선 비용: "+ result[i]);
-        //}
-        
-        // 최단 경로 중 최댓값 찾기
-        int maxValue = 0;
-        for (int i = 1; i < result.length; i++) {
-            maxValue = Math.max(maxValue, result[i]);
-        }
-        
-        // 최댓값과 동일한 비용 찾기
-        for (int i = 1; i < result.length; i++) {
-            if (maxValue == result[i]) {
-                answer ++;
-            }
+        // 최단 거리 배열 초기화
+        distance = new int[n+1];
+        for (int i = 1; i <= n; i++) {
+            distance[i] = Integer.MAX_VALUE;
         }
         
         
-        return answer;
-    }
-    
-    private void bfs(boolean[] visited, int start) {
-        Queue<Integer> q= new LinkedList<>();
+        // 시작 지점 세팅
+        pq.add(new int[] {1, 0});  //시작 노드 (1, 거리: 0)
+        distance[1] = 0;
         
-        // 초기값 세팅
-        visited[start] = true;
-        q.add(start);
-        result[start] = 0;
-        
-        // BFS 수행
-        while (!q.isEmpty()) {
-            int node = q.poll();
-            int nodeValue = result[node];
+        while (!pq.isEmpty()) {
+            int[] now = pq.poll();
+            int nowNode = now[0];
+            int nowDistance = now[1];
             
-            // 인접 노드 탐색
-            for (Integer nextNode : graph[node]) {
-                // 아직 방문하지 않은 노드이면 이동
-                if (!visited[nextNode]) {
-                    result[nextNode] = nodeValue + 1;
-                    q.add(nextNode);
-                    visited[nextNode] = true;
+            // 이미 방문한 노드면 패스
+            if (visited[nowNode]) {
+                continue;
+            }
+            
+            visited[nowNode] = true;
+            
+            for (Integer nextNode : graph[nowNode]) {
+                if(distance[nextNode] > nowDistance + 1) {
+                    distance[nextNode] = nowDistance+1;
+                    pq.add(new int[] {nextNode, distance[nextNode]});
                 }
             }
         }
+        
+        
+        // 가장 큰 수 찾기
+        int maxValue = 0;
+        for (int i = 1; i < distance.length; i++) { 
+            maxValue = Math.max(distance[i], maxValue);
+        }
+        
+        for (int i = 1; i < distance.length; i++) { 
+            if (maxValue == distance[i]){
+                answer++;
+            }
+        }           
+        
+        return answer;
     }
-    
-    
 }
 
 
