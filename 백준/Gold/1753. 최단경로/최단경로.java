dce1f5803165d12bb93c098b889f202ac748986e
ego2 <57621519+jungbin97@users.noreply.github.com>
2025-01-26 16:20:28 +0900

    [Gold IV] Title: 최단경로, Time: 796 ms, Memory: 109116 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -2,95 +2,94 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    // 노드 개수 
-    // 간선 개수
-    static int V, E;
-    // 그래프 인접 리스트
     static ArrayList<Node>[] graph;
-    // 우선순위 큐
-    static PriorityQueue<Node> q = new PriorityQueue<>();
-    // 거리 배열
     static int[] distance;
+    static boolean[] visited;
+    static PriorityQueue<Node> q = new PriorityQueue<>();
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        // 노드 입력 받기
-        V = Integer.parseInt(st.nextToken());
         
-        // 에지 입력 받기
-        E = Integer.parseInt(st.nextToken());
-
-        // 시작 노드 입력 받기
-        int start = Integer.parseInt(br.readLine());
-
-        // 그래프 초기화
-        graph = new ArrayList[V+1];
-        for (int i = 1; i <= V; i++) {
-            graph[i] = new ArrayList<Node>();
-        }
+        // 노드, 엣지 개수
+        int N = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
+            
         
-        for (int i = 0; i < E; i++) {
-            st = new StringTokenizer(br.readLine());
-            // u에서 v까지 w 비용
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
-            graph[u].add(new Node(v, w));
-        }
-
-        // 거리 배열 초기화(무한)
-        distance = new int[V+1];
-        for (int i = 0; i <= V; i++) {
+        // 시작 노드
+        int K = Integer.parseInt(br.readLine());
+        
+        
+        // 최단 거리 배열 초기화
+        distance = new int[N+1];
+        for (int i = 0; i <= N; i++) {
             distance[i] = Integer.MAX_VALUE;
         }
         
-        // 시작 노드 세팅
-        q.add(new Node(start, 0));
-        distance[start] = 0;
-        // 다익스트라 수행
-        while (!q.isEmpty()) {
-            // 큐 뺴기
-            Node now = q.poll();
-            int now_vertex = now.vertex;
+        // 방문 체크 배열
+        visited = new boolean[N+1];
 
-            // 현재 노드의 거리가 큐에서 꺼낸 거리 보다 작으면 이미 갱신된것. 
-            if (distance[now_vertex] < now.value) continue;
+        graph = new ArrayList[N+1];
+        for (int i = 0; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        
+        // 인접 리스트 초기화
+        for (int i = 0; i < E; i++)  {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int value = Integer.parseInt(st.nextToken());
+            graph[start].add(new Node(end, value));
+        }
+        
+        // K를 시작점으로 설정
+        q.add(new Node(K, 0));
+        distance[K] = 0;
+        
+        while (!q.isEmpty()) {
+            Node node = q.poll();
+            int currentNode = node.node;
             
-            for (Node temp : graph[now_vertex]) {
-                int next_vertex = temp.vertex;
-                int next_value = temp.value;
+            if (visited[currentNode]) {
+                continue;
+            }
+            visited[currentNode] =true;
+            
+            for (int i = 0; i < graph[currentNode].size(); i++) {
+                Node nextNode = graph[currentNode].get(i);
+                int next = nextNode.node;
+                int nextValue = nextNode.value;
                 
-                if (distance[next_vertex] > now.value + next_value) {
-                    distance[next_vertex] = now.value + next_value;
-                    q.add(new Node(next_vertex, distance[next_vertex]));
+                if (distance[next] > distance[currentNode] + nextValue) {
+                    distance[next] = distance[currentNode] + nextValue;
+                    q.add(new Node(next, distance[next]));
                 }
             }
         }
-        
-        // 출력
-        for (int i = 1; i<= V; i++) {
-            if (distance[i] == Integer.MAX_VALUE) {
-                System.out.println("INF");
-            } else {
+
+        for (int i = 1; i <= N; i++) {
+            if (visited[i]) {
                 System.out.println(distance[i]);
+            } else {
+                System.out.println("INF");
             }
         }
-
+        
     }
     
     static class Node implements Comparable<Node> {
-        int vertex;
+        int node;
         int value;
-        public Node(int vertex, int value) {
-            this.vertex = vertex;
+
+        Node (int node, int value) {
+            this.node = node;
             this.value = value;
         }
-
+        
+        // value 기준으로 오름차순 정렬
         @Override
-        // 오름차순 정렬
-        public int compareTo(Node o) {
-            return Integer.compare(value, o.value);
+        public int compareTo(Node node) {
+            return Integer.compare(this.value, node.value);
         }
     }
 }
