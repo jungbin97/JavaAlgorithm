4ecf7c808151af8abea4ce2f2304f74dfeb93a14
ego2 <57621519+jungbin97@users.noreply.github.com>
2025-01-27 23:29:26 +0900

    [Platinum IV] Title: K번째 최단경로 찾기, Time: 896 ms, Memory: 100648 KB -BaekjoonHub

--- 백준/Platinum/1854. K번째 최단경로 찾기/K번째 최단경로 찾기.java
+++ 백준/Platinum/1854. K번째 최단경로 찾기/K번째 최단경로 찾기.java
@@ -1,93 +1,103 @@
 import java.util.*;
 import java.io.*;
 
+
 public class Main {
-    // 도시수, 도로수, K
     static int N, M, K;
+    static ArrayList<Node>[] graph;
+    static boolean[] visited;
+    static PriorityQueue<Integer>[] distance;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        // 도시수
+
         N = Integer.parseInt(st.nextToken());
-        // 간선 개수
         M = Integer.parseInt(st.nextToken());
-        // K 번쨰 최단 경로 입력
         K = Integer.parseInt(st.nextToken());
         
-        // 인접리스트 그래프 입력
-        ArrayList<Node>[] graph = new ArrayList[N+1];
+        graph = new ArrayList[N+1];
         for (int i = 0; i <= N; i++) {
             graph[i] = new ArrayList<>();
         }
-        
-        // 간선 입력 받기
+
+        // a에서 b로 가는 비용 c
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            // a에서 b로가는 비용 c
-            int a = Integer.parseInt(st.nextToken()); 
-            int b = Integer.parseInt(st.nextToken()); 
-            int c = Integer.parseInt(st.nextToken()); 
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
             graph[a].add(new Node(b, c));
         }
-        
-        PriorityQueue<Integer>[] distanceQueue = new PriorityQueue[N+1];
-        Comparator<Integer> cp = new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                return o1 < o2 ? 1 : -1;
-            }
-        };
-        
+
+        // 최단 경로우선순위 큐
+        distance = new PriorityQueue[N+1];
         for (int i = 0; i <= N; i++) {
-            distanceQueue[i] = new PriorityQueue<>(K, cp);
+            // 크기 K로, 내림차순 정렬
+            distance[i] = new PriorityQueue<>(K, (o1, o2) -> Integer.compare(o2, o1));
         }
         
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        pq.add(new Node(1, 0));
-        distanceQueue[1].add(0);
-        
         // 다익스트라 수행
+        dijkstra();
+        //
+        // K번쨰 경로 출력
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        for (int i = 1; i <= N; i++) {
+            if (distance[i].size() == K) {
+                bw.write(distance[i].peek() + "\n");
+            } else {
+                bw.write(-1+"\n");
+            }
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+    
+    public static void dijkstra() throws Exception {
+        // // 방문 체크 배열
+        // visited = new boolean[N+1];
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        
+        // 시작 노드 세팅
+        pq.add(new Node(1, 0));
+        distance[1].add(0);
+        
         while (!pq.isEmpty()) {
-            Node now = pq.poll();
-            int currentNode = now.node;
-            int currentCost = now.cost;
+            Node u = pq.poll();
+            int nowNode = u.targetNode;
+            int nowValue = u.value;
             
-            for (Node next : graph[currentNode]) {
-                int nextNode = next.node;
-                int nextCost = currentCost + next.cost;
-                
-                // K개의 최단 경로 유지
-                if (distanceQueue[nextNode].size() < K) {
-                    distanceQueue[nextNode].add(nextCost);
-                    pq.add(new Node(nextNode, nextCost));
-                } else if (distanceQueue[nextNode].peek() > nextCost) {
-                    distanceQueue[nextNode].poll();
-                    distanceQueue[nextNode].add(nextCost);
-                    pq.add(new Node(nextNode, nextCost));
+            for (Node next : graph[nowNode]) {
+                // 저장된 경로가 K개가 아니면 그냥 추가
+                if (distance[next.targetNode].size() < K) {
+                    distance[next.targetNode].add(nowValue+next.value);
+                    pq.add(new Node(next.targetNode, nowValue+next.value));
+                }
+                // 저장된 경로가 K개이고, 현재 가장 큰값보다 작을 때 추가
+                else if (distance[next.targetNode].peek() > nowValue + next.value) {
+                    distance[next.targetNode].poll();
+                    distance[next.targetNode].add(nowValue + next.value);
+                    pq.add(new Node(next.targetNode, nowValue + next.value));
                 }
             }
         }
         
-        // 결과 출력
-        for (int i = 1; i <= N; i++) {
-            if (distanceQueue[i].size() == K) {
-                System.out.println(distanceQueue[i].peek());
-            } else {
-                System.out.println(-1);
-            }
-        }
     }
-    
-    static class Node implements Comparable<Node> { 
-        int node;
-        int cost;
-        public Node(int node, int cost) {
-            this.node = node;
-            this.cost = cost;
+
+    public static class Node implements Comparable<Node> {
+        int targetNode;
+        int value;
+
+        Node (int targetNode, int value) {
+            this.targetNode = targetNode;
+            this.value = value;
         }
+
+        // 오름차순 정렬
         @Override
         public int compareTo(Node o) {
-            return this.cost < o.cost ? -1 : 1;
+            return Integer.compare(this.value, o.value);
         }
     }
 }
