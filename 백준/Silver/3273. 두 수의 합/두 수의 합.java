1d2c653cd32faaf22af59d67548e3d25a87cbb1f
ego2 <57621519+jungbin97@users.noreply.github.com>
2025-02-06 00:51:35 +0900

    [Silver III] Title: 두 수의 합, Time: 260 ms, Memory: 26700 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -8,35 +8,24 @@ public class Main {
         
         int n = Integer.parseInt(br.readLine());
         int[] arr = new int[n];
+        boolean[] check = new boolean[2000001];
         StringTokenizer st = new StringTokenizer(br.readLine());
         
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        // 정렬 오름차순
-        Arrays.sort(arr);
 
         int target = Integer.parseInt(br.readLine());
-        
-        // 투포인터 인덱스 지정
-        int start = 0;
-        int end = n-1;
         int result = 0;
         
-        while (start < end) {
-            int sum = arr[start]+ arr[end];
-            // target 값과 일치하면 start + 1, end - 1
-            if (target == sum) {
-                start++;
-                end--;
+        // 투포인터 없이, 공간복잡도 활용
+        for (int i = 0; i < n; i++) {
+            if (target-arr[i] > 0 && check[target-arr[i]]) {
                 result++;
-            // target 값 보다 크다면, end--
-            } else if (target < sum) {
-                end--;
-            } else {
-                start++;
             }
+            check[arr[i]] = true;
         }
+
         
         System.out.println(result);
     }
@@ -45,5 +34,11 @@ public class Main {
 
 // 정렬?
 // 1 2 3 5 7 9 10 11 12
+// target = 13
 
-
+// 정렬 없이
+// 5 12 7 10 9 1 2 3 11
+// target = 13
+// 13 - 5 => 7 => 아직체크안함 pass => check[5] = true
+// ... 
+// 13 - 7 = 5 => 있음 => count++
