402041d1b0c3a619896f2b2f73cfe5d07d6a065a
ego2 <57621519+jungbin97@users.noreply.github.com>
2025-02-08 21:47:08 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 280 ms, Memory: 20708 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -8,26 +8,48 @@ public class Main {
         int n = sc.nextInt();
         int k = sc.nextInt();
 
-        Queue<Integer> q = new LinkedList<>();
+        int[] pre = new int[5001];
+        int[] next = new int[5001];
 
+        // 결과 저장
+        ArrayList<Integer> result = new ArrayList<>();
+
+        int len = 0;
+        // 그처음 노드와 마지막 노드가 서로키서로 가르키도록 지정
         for (int i = 1; i <= n; i++) {
-            q.add(i);
+            pre[i] = (i==1) ? n : i-1;
+            next[i] = (i==n) ? 1 : i+1;
+            len++;
         }
 
-        StringBuffer sb = new StringBuffer();
-        sb.append('<');
-
-        while (q.size() > 1) {
-            for (int i = 0; i < k-1; i++) {
-                int val = q.poll();
-                q.add(val);
+        int count = 1;
+        for (int cur = 1; len != 0; cur = next[cur]) {
+            // K번째일때 제거
+            if (count == k) {
+                // 현재 노드 제거
+                pre[next[cur]] = pre[cur];
+                next[pre[cur]] = next[cur];
+                count = 1;
+                len--;
+                result.add(cur);
+            } else {
+                count++;
             }
+        }
+        
+        // 출력
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
+        for (int i = 0; i < result.size(); i++) {
+            if (i == result.size()-1) {
+                sb.append(result.get(i) + ">");
+                break;
+            }
+            sb.append(result.get(i)+ ", ");
 
-            sb.append(q.poll()).append(", ");
         }
 
-        sb.append(q.poll()).append('>');
-        System.out.println(sb);
+        System.out.println(sb.toString());
     }
 }
 
@@ -37,3 +59,7 @@ public class Main {
 
 // 456712
 // => 6
+
+//     1 2 3 4 5 6 7
+//pre  7 1 2 3 4 5 6
+//next 2 3 4 5 6 7 1
