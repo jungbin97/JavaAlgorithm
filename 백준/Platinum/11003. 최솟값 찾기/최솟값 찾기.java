02327841ebec3891383d2991c732f14bf8de90ce
ego2 <57621519+jungbin97@users.noreply.github.com>
2025-02-11 19:36:23 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 2188 ms, Memory: 536540 KB -BaekjoonHub

--- 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
@@ -8,37 +8,54 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
-        int L = Integer.parseInt(st.nextToken()); 
+        int L = Integer.parseInt(st.nextToken());
         
-        Deque<Node> mydeque = new LinkedList<>();
+        Deque<Node> dq = new ArrayDeque<>();
+
         st = new StringTokenizer(br.readLine());
-        
-        // 덱 초기화
-        for(int i = 0; i < N; i++) {
+        for (int i = 0; i < N; i++) {
             int now = Integer.parseInt(st.nextToken());
 
-            // 덱의 마지막 위치에서 부터 now 보다 큰 값은 덱에서 제거
-            while (!mydeque.isEmpty() && mydeque.getLast().value > now) {
-                mydeque.removeLast();
+            while (!dq.isEmpty() && dq.peekLast().value > now) {
+                dq.removeLast();
             }
-            mydeque.addLast(new Node(now, i));
-            // 범위 벗어난 값은 덱에서 제거
-            if (mydeque.getFirst().index <= i - L) {
-                mydeque.removeFirst();
+            
+            dq.add(new Node(now, i));
+            
+            // 현재 인덱스 범위계산 후 초과 노드 제거
+            while (dq.peekFirst().index <= i - L){
+                dq.removeFirst();
             }
-            bw.write(mydeque.getFirst().value + " ");
+            
+            bw.write(dq.peekFirst().value + " ");
         }
+
         bw.flush();
         bw.close();
     }
-    
+
     static class Node {
-        public int value;
-        public int index;
-        
-        Node(int value, int index) {
-            this.index = index;
+        int value;
+        int index;
+
+        public Node (int value, int index)  {
             this.value = value;
+            this.index = index;
         }
     }
 }
+// A1 A2 A3 A4  A5 A6 A7
+// 1  5  2   3  6   2  3 
+
+
+// D_1 = A_(1-3+1) ~ A_1 => A_0 ~ A_1 => 1
+// D_2 = A_(2-3+1) ~ A_2 => A_0 ~ A_2 => 1
+// D_3 = A_(3-3+1) ~ A_3 => A_1 ~ A_3 => 1
+// D_4 = A_(4-3+1) ~ A_4 => A_2 ~ A_4 => 2
+
+// java 1초에 1억번(100_000_000) 연산 
+// 5_000_000번 연산(시간복잡도 주의)
+// 특정 구간에서의 최소값을 구해야함.
+
+// 오른쪽 하나씩늘어남. 왼쪽도 하나씩 줄음. => 우선순위 큐 (logN) log5 + log1000 + log1000 = 20(대략)
+// 대략 100_000_000(1억)번 연산
