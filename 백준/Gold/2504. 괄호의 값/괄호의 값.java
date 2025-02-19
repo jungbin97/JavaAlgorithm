f311a1b5496e4a3ad2848cd98d6f89268b0cfbb7
ego2 <57621519+jungbin97@users.noreply.github.com>
2025-02-19 20:18:28 +0900

    [Gold V] Title: 괄호의 값, Time: 104 ms, Memory: 14352 KB -BaekjoonHub

--- 백준/Gold/2504. 괄호의 값/괄호의 값.java
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.java
@@ -2,87 +2,74 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int[] arr;
-    static int[] visited;
-    static int[] isCycle;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        String line = br.readLine();
         Stack<Character> stack = new Stack<>();
+
+        String line = br.readLine();
         
-        // 더하기 연산 저장
-        int sum = 0;
-        // 곱하기 연산 저장
+        boolean escapeFlag = false;
+        int sumValue = 0;
         int temp = 1;
-        for (int i = 0; i < line.length(); i++){
+        for (int i = 0; i < line.length(); i++) {
             char c = line.charAt(i);
-            
-            if (c == '(') {
+            if (c == '('){
                 stack.push(c);
                 temp *= 2;
             } else if (c == '[') {
                 stack.push(c);
-                temp *= 3;
-            } else if (c == ']'){
-                // 스택이 비어있다면 || 짝이 맞지 않다면
-                if (stack.isEmpty() || stack.peek() != '[') {
-                    System.out.println("0");
-                    return;
-                }
-                // 이전 값과 짝이 맞는 경우
-                if (line.charAt(i-1) == '[') {
-                    sum += temp;
-                }
-                stack.pop();
-                temp /= 3;
-
-            } else {
-                // 스택이 비어있다면 || 짝이 맞지 않다면
+                temp *= 3; 
+            } else if (c == ')') {
                 if (stack.isEmpty() || stack.peek() != '(') {
-                    System.out.println("0");
-                    return;
+                    escapeFlag = true;
+                    break;
                 }
-                // 이전 값과 짝이 맞는 경우
+                
+                // 바로 이전 값이 '('
                 if (line.charAt(i-1) == '(') {
-                    sum += temp;
+                    sumValue += temp;
                 }
                 stack.pop();
-                temp /= 2;
+                temp/=2;
+            } else if (c == ']') {
+                if (stack.isEmpty() || stack.peek() != '[') {
+                    escapeFlag = true;
+                    break;
+                }
+                
+                // 바로 이전 값이 '['
+                if (line.charAt(i-1) == '[') {
+                    sumValue += temp;
+                }
+                stack.pop();
+                temp/=3;
             }
-            
         }
         
-        // 스택에 남아있다면 잘못된 문자열
-        if (!stack.isEmpty()) {
+        // 스택이 비어있지 않다면, escapeFlag true
+        if (!stack.isEmpty() || escapeFlag) {
             System.out.println("0");
-            return;
+        } else {
+            System.out.println(sumValue);
         }
-        
-        System.out.println(sum);
     }
 }
 
-// ([]()) 2 X (3 + 2)=  10
-// (2 X 3) + (2 X 2)
+// "("인경우 stack.push()
+// temp *= 2
+// "["인경우 stack.push()
+// temp *= 3
+// "]"인경우
+// 스택이 비엇거나 || stack.peek() != '[' 짝이 안맞는 경우 탈출 0 반환
+// 이전값이 '['이면 
+// sumValue += temp
+// stack.pop()
+// temp / 3
+// 아니면
+// stack.pop()
+// temp/ 3
+// ....
 
-// sum = 0;
-// temp = 2 * 3
-// sum += temp => 6
-// temp / 3 = 2
-// 
-// temp = 2 * 2 = 4
-// sum += temp => 6 + 4 = 10
-// temp / 2 = 2
-
-// temp / 2 = 2
-
-
-// '(', '['면 stack.push()
-// ')', ']'면 stack.peek()와 짝이 맞는지확인, 
-// 짝이 맞지 않으면 0반환,, 
-// 스택이 비어있는지 확인 비어있다면 0 반환
-// 
-// 짝이 맞고, 이전 '['이고 현재 ']'이면 붙어있는 것. (sum += 괄호값),  temp // 괄호 값
-// 짝이 맞고, 이전 값이 다른 것. 이면 sum과 temp  곱하기
+// 모든 문자열 순회 후, 스택에 괄호가 남아있다면 0반환
+// 아니라면 값 출력
 
