d1d5ee4ed0ee600d6e579ea0fcac6dcb55031aea
ego2 <57621519+jungbin97@users.noreply.github.com>
2025-03-03 15:47:01 +0900

    [Silver V] Title: 수 정렬하기 5, Time: 18068 ms, Memory: 110328 KB -BaekjoonHub

---
+++ 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int[] arr;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int n = Integer.parseInt(br.readLine());
+        int MAX = 1_000_000;
+        int[] arr = new int[MAX *2+1];
+        for (int i = 0; i < n; i++) {
+            int value = Integer.parseInt(br.readLine());
+            arr[value + MAX] += 1;
+        }
+        
+        for (int i = 0; i < MAX*2+1; i++) {
+            if (arr[i] != 0) {
+                while (arr[i]-- > 0) {
+                    bw.write(i-MAX + "\n");
+                }
+            }
+        }
+        
+        bw.flush();
+        bw.close();
+    }
+}
