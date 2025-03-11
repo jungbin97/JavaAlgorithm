5cdd8ffe5993677b397fb90c09763f5263222ba7
ego2 <57621519+jungbin97@users.noreply.github.com>
2025-03-11 23:30:08 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 100 ms, Memory: 14032 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -3,39 +3,36 @@ import java.io.*;
 
 public class Main {
     static int result = 0;
-    static int[] arr;
+    static int[] D = new int[12];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        arr = new int[] {1, 2, 3};
-        
         int tc = Integer.parseInt(br.readLine());
         
+        D[1] = 1;
+        D[2] = 2;
+        D[3] = 4;
+        for (int i = 4; i <= 11; i++) {
+            D[i] = D[i-1] + D[i-2] + D[i-3];
+        }
         for (int t = 0; t < tc; t++) {
             int n = Integer.parseInt(br.readLine());
-            
-            func(n, 0);
-            System.out.println(result);
-            result = 0;
+            System.out.println(D[n]);
         }
         
-    }
-    
-    static void func(int n, int sum) {
-        // base condtion
-        if (sum == n) {
-            result++;
-            return;
-        }
-        
-        if (sum > n) {
-            return;
-        }
-        
-        for (int num : arr) {
-            func(n, sum+num);
-        }
     }
 }
 
+// 점화식 구하기
+// D[4] = ?
+// 1+1+1+1, 3+1, 2+1+1, 1+2+1 => D[3] +1
+// 1+1+2, 2+2 => D[2]+1
+// 1+3 => D[1]+1
 
+// D[4] = D[3]+D[2]+D[1]
+// D[i] = D[i-1] + D[i-2] + D[i-3]
+
+// 초기값
+// D[1] = 1
+// D[2] = 2
+// D[3] = 4
