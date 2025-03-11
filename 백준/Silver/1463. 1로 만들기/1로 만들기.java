440e8ba381108169200e62eaf10006b03f305292
ego2 <57621519+jungbin97@users.noreply.github.com>
2025-03-11 22:53:25 +0900

    [Silver III] Title: 1로 만들기, Time: 120 ms, Memory: 18168 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -2,20 +2,35 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        int n = sc.nextInt();
+        int n = Integer.parseInt(br.readLine());
+        int[] DP = new int[n+1];
         
-        int[] D = new int[n+1];
-        
-        D[1] = 0;
-        
+        DP[1] = 0;
         for (int i = 2; i <= n; i++) {
-            D[i] = D[i-1] + 1;
-            if (i % 2 == 0) D[i] = Math.min(D[i], D[i/2] + 1);
-            if (i % 3 == 0) D[i] = Math.min(D[i], D[i/3] + 1);
+            DP[i] = DP[i-1]+1;
+            if (i % 2 == 0) {
+                DP[i]=Math.min(DP[i/2]+1, DP[i]);
+            }
+            if (i % 3 == 0) {
+                DP[i]=Math.min(DP[i/3]+1, DP[i]);
+            }
         }
-        System.out.println(D[n]);
+        System.out.println(DP[n]);
     }
 }
+
+// D[12] = ? 
+// D[4] + 1
+// D[6] + 1
+// D[11] + 1
+// D[12] = min(D[4] + 1, D[6] + 1, D[11] + 1)
+// 
+// 
+// 점화식 : D[k] = min(D[k/3]+1, D[k/2]+1, D[k-1]+1)
+// 초기값 정의: D[1] = 0
+//
+// D[2] = min(D[2/3]+1, D[k/2]+1, D[2-1+1]) = (1, 1, 1)
+// D[3] = min(D[3/3]+1, D[3/2]+1, D[2]+1) = (2, 2, 2)
