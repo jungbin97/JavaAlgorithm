6404037b72fddfc23f7c7c7581d89e3167d1d078
ego2 <57621519+jungbin97@users.noreply.github.com>
2025-03-18 18:22:15 +0900

    [Silver III] Title: 이친수, Time: 104 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.java
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -2,21 +2,57 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        int N = sc.nextInt();
+        int n = Integer.parseInt(br.readLine());
         
-        long[] DP = new long[91];
+        long[][] D = new long[n+1][2];
         
-        DP[0] = 0;
-        DP[1] = 1;
-        DP[2] = 1;
-
-        for (int i = 3; i <= N; i++) {
-            DP[i] = DP[i-1] + DP[i-2];
+        // 초기값 세팅하기
+        D[1][0] = 0;
+        D[1][1] = 1;
+        
+        for (int i = 2; i <=n; i++) {
+            D[i][0] = D[i-1][0] + D[i-1][1];
+            D[i][1] = D[i-1][0];
         }
         
-        System.out.println(DP[N]);
+        System.out.println(D[n][0]+D[n][1]);
     }
 }
+
+
+// 테이블 정의하기
+// D[i][j] i자리의 j로 끝날 경우 이친수 개수
+
+// 점화식 구하기
+// D[0][0] = 0
+// D[0][1] = 0
+// D[1] = ? 
+// D[1][0] = 0
+// D[1][1] = 1
+
+
+// D[2] = ?
+// D[2][0] = 10 => 1가지
+// D[2][1] = 0 =
+// D[2][0] + D[2][1] => 1가지
+
+// D[3] = ? = 100, 101 => 2가지
+// D[3][0] = 100 => 1가지
+// D[3][1] = 101 => 1가지
+// D[3][0] = D[2][0]+D[2][1] => 1가지
+// D[3][1] = D[2][0] => 1가지
+
+
+// D[4] = 1000, 1001, 1010 => 3가지
+// D[4][0] = D[3][0]+D[3][1] = 2가지
+// D[4][1] = D[3][0] = 1가지
+
+// D[i][0] = D[i-1][0]+D[i-1][1]
+// D[i][1] = D[i-1][0]
+
+// 초기값 세팅하기
+// D[1][0] = 0
+// D[1][1] = 1
