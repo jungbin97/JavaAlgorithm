db56de6cc027da2154ddd5b4574368a28bdb7ec3
ego2 <57621519+jungbin97@users.noreply.github.com>
2025-03-19 15:44:40 +0900

    [Silver II] Title: 연속합, Time: 232 ms, Memory: 23800 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -2,36 +2,38 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
+        
+        int n = Integer.parseInt(br.readLine());
+        int arr[] = new int[n+1];
+        int[] D = new int[n+1];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
+        for (int i= 1; i <= n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
         
-        int[] dp = new int[N];
-        int max = arr[0];
-        dp[0] = arr[0];
-        
-        for (int i = 1; i < N; i++) {
-            dp[i] = Math.max(arr[i], dp[i-1] + arr[i]);
-            max = Math.max(dp[i], max);
+        for (int i = 1; i <=n; i++) {
+            D[i] = Math.max(arr[i], D[i-1]+arr[i]);
         }
 
-        System.out.println(max);
+        int result = -1000;
+        for (int i = 1; i <= n; i++) {
+            result = Math.max(D[i], result);
+        }
+        
+        System.out.println(result);
     }
 }
 
 
 
-//     [10 -4 3 1 5 6]
-// dp= [10, 6, 9, 10, 15, 21]
-// (arr[i], dp[i-1], arr[i])
+// 테이블 정의하기 
+// D[i] i번째로 끝나는 연속항 최대 값
 
-
-
-
+// 점화식 구하기
+// D[i] = max(0, D[i-1]) + arr[i]
+// 
+// 초기값 세팅
+// 시작 1부터
