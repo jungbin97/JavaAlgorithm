48ccf38ee92b7f09d2585681183ced606e5505b7
ego2 <57621519+jungbin97@users.noreply.github.com>
2025-03-20 14:38:37 +0900

    [Silver III] Title: 퇴사, Time: 100 ms, Memory: 14348 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -2,28 +2,29 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+
+        int[] T = new int[n+1];
+        int[] P = new int[n+1];
+        int[] D = new int[n+2];
         
-        int N = sc.nextInt();
-        // 0 | 1 2 3 4 5 6 7 | 8
-        int[] D = new int[N+2];
-        
-        int[] T = new int[N+1];
-        int[] P = new int[N+1];
-        
-        for (int i = 1; i <= N; i++) {
-            T[i] = sc.nextInt();
-            P[i] = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            T[i] = Integer.parseInt(st.nextToken());
+            P[i] = Integer.parseInt(st.nextToken());
         }
         
-        for (int i = N; i > 0; i--) {
-            if (i + T[i] > N+1) {
-                D[i] = D[i+1];
+        for (int i = n; i >= 1; i--) {
+            if (i + T[i] <= n+1) {
+                D[i] = Math.max(P[i] + D[i+T[i]], D[i+1]);
             } else {
-                D[i] = Math.max(D[i+1], P[i] + D[i+T[i]]);
+                D[i] = D[i+1];
             }
         }
-        System.out.println(D[1]);
+        
+        System.out.print(D[1]);
     }
 }
