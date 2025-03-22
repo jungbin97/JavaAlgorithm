6f2ed5d93d8143f2ead33d73e3d54aaf4037f57d
ego2 <57621519+jungbin97@users.noreply.github.com>
2025-03-22 19:07:37 +0900

    [Gold V] Title: 퇴사 2, Time: 680 ms, Memory: 321920 KB -BaekjoonHub

---
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        int[] T = new int[n+1];
+        int[] P = new int[n+1];
+        long[] D = new long[n+2];
+
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            T[i]= Integer.parseInt(st.nextToken());
+            P[i]= Integer.parseInt(st.nextToken());
+        }
+        
+        for (int i = n; i > 0; i--) {
+            if (i+T[i] <= n+1) {
+                D[i] = Math.max(P[i]+D[i+T[i]], D[i+1]);
+            } else {
+                D[i] = D[i+1];
+            }
+        }
+
+        System.out.println(D[1]);
+        
+    }
+}
+
+// i+T[i] <= n+1
+// max(P[i]+D[i+T[i]], D[i+1]) 
